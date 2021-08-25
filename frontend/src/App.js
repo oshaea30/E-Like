27f79611d433d29d3afd984ec3e7054361ae74c0
Oshae Alexis <terongp3@gmail.com>
2021-08-25 12:42:04 -0400

    updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,10 @@
-import React from 'react';
-import Header from './components/Header';
-import TinderCards from './components/TinderCards';
-import SwipeButtons from './components/SwipeButtons';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './assets/App.css';
+import React from 'react'
+import Router from './Router'
+import "./assets/style.css"
 
 function App() {
   return (
-    <div className="App">  
-     {/* *** Header *** */}
-     <Header />
-     <Router>
-      <Switch>
-        <Route path = '/chat'>
-          <h1>I am the chat page</h1>
-        </Route>
-        <Route path = '/'>
-          <TinderCards />
-          <SwipeButtons />
-        </Route>
-      </Switch>
-     </Router>
-     {/* *** Tinder Cards *** */}
-     {/* *** Buttons below tinder cards *** */}
-
-     {/* *** Chat screen *** */}
-     {/* *** Individual chat screen *** */}
-    </div>
+    <Router />
   );
 }
 
