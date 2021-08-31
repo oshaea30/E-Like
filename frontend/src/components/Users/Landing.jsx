d8989282319a34d307d986cfce96aea92f15e93a
Oshae Alexis <terongp3@gmail.com>
2021-08-31 02:08:35 -0400

    updated frontend

---
+++ frontend/src/components/Users/Landing.jsx
@@ -0,0 +1,14 @@
+<body>
+<div class="landing-page">
+  <div class="content">
+    <img src="img/icon.svg" alt="" />
+    <h1>E-like</h1>
+    <p>
+      Match and chat with people you like<br />
+      and that are like you!
+    </p>
+    <button class="btn green-btn">Log In</button>
+    <button class="btn">Sign Up</button>
+  </div>
+</div>
+</body>
