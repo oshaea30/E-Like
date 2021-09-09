29e0a800d3b222ead4f43dc080413d339eee7fcc
l-panhavoan <l-sovanpanhavoan@beniten.com>
2021-09-09 15:34:51 +0700

    modify chat api

--- backend/apps/chats/serializers.py
+++ backend/apps/chats/serializers.py
@@ -1,9 +1,11 @@
+from apps.users.serializers import UserSerializer
 from .models import Chat
 from rest_framework import serializers
 
-
 class ChatSerializer(serializers.ModelSerializer):
-
+    send_user_id = UserSerializer(many=False, read_only= True)
+    receive_user_id = UserSerializer(many=False, read_only= True)
     class Meta:
         model = Chat
         fields = '__all__'
+        depth = 1
