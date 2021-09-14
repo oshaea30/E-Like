7ae5556fd09a13b404b6c9e805980ea1d31d01ad
l-panhavoan <l-sovanpanhavoan@beniten.com>
2021-09-14 17:26:51 +0700

    update page size back to 21

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -168,5 +168,5 @@ cloudinary.config(
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 7
+    'PAGE_SIZE': 21
 }
